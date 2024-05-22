9e76dfaba7a2f478815c8f484fdf8da10152b1be
PJ <peterjoe2005@gmail.com>
2024-05-22 17:50:41 -0400

    Cleaned up code and added comments

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
+// alerts user with userInputs
 function returnText()
 {
     let input = document.getElementById("userInput").value
+    alert(input);
 }
 
+// counts characters of a textarea
 function countCharacters() {
-    var text = document.getElementById("textInput").value;
+    var text = document.getElementById("userInput").value;
     var count = text.length;
     document.getElementById("charCount").innerText = count + " / 500";
 }
